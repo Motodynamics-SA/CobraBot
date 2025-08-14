6e6f80c6bbd0f2a4c73409bb19919ee8829eaba4
Paul Isaris <paulisaris@gmail.com>
2025-08-14 11:05:29 +0300

    db prefix

--- config/database.php
+++ config/database.php
@@ -108,7 +108,7 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'prefix' => 'cobrabot.',
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
